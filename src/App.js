b697a3d4db79ae6224ed0410ee8dfe80bf8cd378
Casey Olson <casey@bookcasey.com>
2022-08-15 11:16:42 -0400

    post morning lecture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    async function getTodos() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await response.json();
+
+      setTodos(data);
+    }
+
+    getTodos();
+  }, []);
+
+  useEffect(() => { 
+    document.title = `Current todo draft: ${title}`;
+  }, [title]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    async function postTodo() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: "POST",
+        body: JSON.stringify({
+          title: title,
+        })
+      });
+      
+      console.log(response);
+    }
+
+    await postTodo();
+    setTitle("");
+
+  }
+  
+  
+  console.log('Inside component body');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label> New todo title:
+          <input name='title' value={title} onChange={(event) => setTitle(event.target.value)}></input>
+        </label>
+        <button>Submit</button>
+      </form>
+      {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
     </div>
   );
 }
